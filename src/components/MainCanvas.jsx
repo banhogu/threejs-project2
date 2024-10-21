e7943a4114eeb2a5c39c1b3e7c68af311822ae98
hojin <devbang5674@gmail.com>
2024-10-21 19:55:05 +0900

    feat: glb파일 로딩해보기

--- src/components/MainCanvas.jsx
+++ src/components/MainCanvas.jsx
@@ -1,13 +1,13 @@
-import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import { Color } from "three";
-import { Meshes } from "./Meshes";
-import { Lights } from "./Lisht";
+import * as THREE from "three";
 
 export const MainCanvas = () => {
   return (
     <Canvas
       gl={{ antialias: true }}
+      // shadows={{ enabled: true, type: THREE.PCFSoftShadowMap }}
+      // shadows={"basic"}
+      // shadows={"percentage"}
       shadows={"soft"}
       camera={{
         fov: 60,
@@ -16,27 +16,12 @@ export const MainCanvas = () => {
         far: 100,
         position: [5, 5, 5],
       }}
-      scene={{ background: new Color(0x000000) }} // 매번 리렌더되므로, 지양해야할 패턴
+      scene={{ background: new THREE.Color(0x000000) }} // 매번 리렌더되므로, 지양해야할 패턴
     >
-      {/* Drei로 컴포넌트화 된 OrbitControls */}
-      <OrbitControls />
-
-      {/* directionalLight 및 간단한 그림자 설정 */}
-      <directionalLight
-        args={[0xffffff, 5]}
-        position={[4, 4, 4]}
-        castShadow
-        shadow-camera-left={-25}
-        shadow-camera-right={25}
-        shadow-camera-top={25}
-        shadow-camera-bottom={-25}
-        shadow-camera-near={0.1}
-        shadow-camera-far={1000}
-        shadow-mapSize-width={4096}
-        shadow-mapSize-height={4096}
-      />
+      <Controls />
       <Lights />
       <Meshes />
+      <GLBModel />
     </Canvas>
   );
 };
